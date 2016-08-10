b24ab11f2306d5b8e6ef8b0a79f4dec8fe847f88
j1angvei <mjiangwei@outlook.com>
2016-08-11 00:12:42 +0800

    move all headers to HeaderUtil

--- app/src/main/java/cn/j1angvei/cnbetareader/data/remote/CnbetaApi.java
+++ app/src/main/java/cn/j1angvei/cnbetareader/data/remote/CnbetaApi.java
@@ -1,51 +1,68 @@
 package cn.j1angvei.cnbetareader.data.remote;
 
-
 import cn.j1angvei.cnbetareader.data.remote.response.BaseResponse;
 import cn.j1angvei.cnbetareader.data.remote.response.CommentResponse;
 import okhttp3.ResponseBody;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 import rx.Observable;
+
+import static cn.j1angvei.cnbetareader.util.HeaderUtil.HEADER_ACCEPT_IMG;
+import static cn.j1angvei.cnbetareader.util.HeaderUtil.HEADER_ACCEPT_JSON;
+import static cn.j1angvei.cnbetareader.util.HeaderUtil.HEADER_AJAX;
+import static cn.j1angvei.cnbetareader.util.HeaderUtil.HEADER_CACHE_MAX;
+import static cn.j1angvei.cnbetareader.util.HeaderUtil.HEADER_CACHE_NO;
+import static cn.j1angvei.cnbetareader.util.HeaderUtil.KEY_REFERER;
 
 /**
  * Created by Wayne on 2016/6/13.
  * cnBeta Api retrieved from fiddler
  */
 public interface CnbetaApi {
+    @Headers(HEADER_AJAX)
     @GET("/more")
     Observable<ResponseBody> getNews(@Query("jsoncallback") String callback, @Query("type") String type, @Query("page") int page, @Query("_") long timestamp);
 
+    @Headers(HEADER_AJAX)
     @GET("/topics/more")
     Observable<ResponseBody> getTopicNews(@Query("jsoncallback") String callback, @Query("id") String topicId, @Query("page") int page, @Query("_") long timestamp);
 
+    @Headers(HEADER_CACHE_MAX)
     @GET("/topics.htm")
     Observable<ResponseBody> getTopics(@Query("letter") String letter);
 
+    @Headers({HEADER_CACHE_NO, "Pragma:no-cache"})
     @GET("/articles/{sid}.htm")
     Observable<ResponseBody> getArticleContent(@Path("sid") String sid);
 
+    @Headers({HEADER_ACCEPT_JSON, HEADER_AJAX})
     @FormUrlEncoded
     @POST("/cmt")
-    Observable<ResponseBody> getArticleComment(@Field("csrf_token") String token, @Field("op") String op);//OP format, "1,sid,sn"
+    Observable<ResponseBody> getArticleComment(@Header(KEY_REFERER) String refer, @Field("csrf_token") String token, @Field("op") String op);//OP format, "1,sid,sn"
 
+    @Headers({HEADER_ACCEPT_JSON, HEADER_AJAX})
     @FormUrlEncoded
     @POST("/comment")
-    Observable<BaseResponse> operateComment(@Field("csrf_token") String token, @Field("op") String action, @Field("sid") String sid, @Field("tid") String tid);// op as "support against report"
+    Observable<BaseResponse> operateComment(@Header(KEY_REFERER) String refer, @Field("csrf_token") String token, @Field("op") String action, @Field("sid") String sid, @Field("tid") String tid);// op as "support against report"
 
+    @Headers({HEADER_ACCEPT_JSON, HEADER_AJAX})
     @FormUrlEncoded
     @POST("/comment")
-    Observable<CommentResponse> publishComment(@Field("csrf_token") String token, @Field("op") String action, @Field("content") String content, @Field("seccode") String captcha, @Field("sid") String sid, @Field("pid") String pid);//op as "publish"
+    Observable<CommentResponse> publishComment(@Header(KEY_REFERER) String refer, @Field("csrf_token") String token, @Field("op") String action, @Field("content") String content, @Field("seccode") String captcha, @Field("sid") String sid, @Field("pid") String pid);//op as "publish"
 
+    @Headers({HEADER_ACCEPT_JSON, HEADER_AJAX})
     @GET("/captcha.htm")
-    Observable<ResponseBody> getCaptchaAddress(@Query("refresh") String refresh, @Query("csrf_token") String token, @Query("_") long timestamp);
+    Observable<ResponseBody> getCaptchaAddress(@Header(KEY_REFERER) String refer, @Query("refresh") String refresh, @Query("csrf_token") String token, @Query("_") long timestamp);
 
+    @Headers(HEADER_ACCEPT_IMG)
     @GET("/captcha.htm")
-    Observable<ResponseBody> getCaptchaImage(@Query("v") String v);
+    Observable<ResponseBody> getCaptchaImage(@Header(KEY_REFERER) String refer, @Query("v") String v);
 
     @GET("/deliver")
     Observable<ResponseBody> getCsrfToken();
--- app/src/main/java/cn/j1angvei/cnbetareader/util/HeaderUtil.java
+++ app/src/main/java/cn/j1angvei/cnbetareader/util/HeaderUtil.java
@@ -4,11 +4,70 @@ package cn.j1angvei.cnbetareader.util;
  * Created by Wayne on 2016/8/9.
  */
 public class HeaderUtil {
-    private static final String PH_SID = "sid";
-    public static final String VALUE_ACCPET_IMG = "image/png, image/svg+xml, image/jxr, image/*;q=0.8, */*;q=0.5";
-    private static final String VALUE_REFERER = "http://www.cnbeta.com/articles/" + PH_SID + ".htm";
+    /**
+     * These Headers will be added using {@link cn.j1angvei.cnbetareader.data.remote.interceptor.AddHeaderInterceptor}
+     */
+    public static final String KEY_CONNECTION = "Connection";
+    public static final String VALUE_CONNECTION = "keep-alive";
 
-    public static String getReferer(String sid) {
-        return VALUE_REFERER.replace(PH_SID, sid);
+    public static final String KEY_LANGUAGE = "Accept-Language";
+    public static final String VALUE_LANGUAGE = "zh-CN,zh;q=0.8";
+
+    public static final String KEY_HOST = "Host";
+    public static final String VALUE_HOST = "www.cnbeta.com";
+
+    public static final String KEY_UA = " User-Agent";
+    public static final String VALUE_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36";
+
+    /**
+     * Cookie will automatically handled by {@link okhttp3.CookieJar}
+     */
+    public static final String KEY_COOKIE = "Cookie";
+
+    /**
+     * If no header in request, this will add to the request in {@link cn.j1angvei.cnbetareader.data.remote.interceptor.AddHeaderInterceptor}
+     */
+    public static final String KEY_REFERER = "Referer";
+    public static final String VALUE_REFER = "http://www.cnbeta.com/";
+
+    public static final String KEY_ACCEPT = "Accept";
+    public static final String VALUE_ACCEPT = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+
+    /**
+     * Specific Headers, which are added in {@link cn.j1angvei.cnbetareader.data.remote.CnbetaApi}
+     */
+    private static final String KEY_AJAX = "X-Requested-With";
+    private static final String VALUE_AJAX = "XMLHttpRequest";
+    private static final String VALUE_ACCEPT_JSON = "application/json, text/javascript, */*; q=0.01";
+    private static final String VALUE_ACCEPT_IMG = "image/webp,image/*,*/*;q=0.8";
+    private static final String KEY_CACHE = "Cache-Control";
+    private static final String VALUE_CACHE_MAX = "max-age=0";
+    private static final String VALUE_CACHE_NO = "no-cache";
+
+    //response is json format
+    public static final String HEADER_ACCEPT_JSON = KEY_ACCEPT + ":" + VALUE_ACCEPT_JSON;
+    //response is image format
+    public static final String HEADER_ACCEPT_IMG = KEY_ACCEPT + ":" + VALUE_ACCEPT_IMG;
+    //request is ajax format
+    public static final String HEADER_AJAX = KEY_AJAX + ":" + VALUE_AJAX;
+    //maximum cache,such as in all topics
+    public static final String HEADER_CACHE_MAX = KEY_CACHE + ":" + VALUE_CACHE_MAX;
+    public static final String HEADER_CACHE_NO = KEY_CACHE + ":" + VALUE_CACHE_NO;
+
+    /**
+     * Headers must be handled by okhttp
+     */
+    private static final String KEY_ENCODING = "Accept-Encoding";
+    private static final String VALUE_ENCODING = "gzip, deflate";
+
+    /**
+     * Dynamic Headers
+     */
+    //need specific string to form the header
+    private static final String PH_SID = "sid";
+    private static final String VALUE_REFERER_CONTENT = "http://www.cnbeta.com/articles/" + PH_SID + ".htm";
+
+    public static String getRefererValue(String sid) {
+        return VALUE_REFERER_CONTENT.replace(PH_SID, sid);
     }
 }
